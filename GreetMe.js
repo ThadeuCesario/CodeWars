ef37c4f7341c454221b02f61a90cf74367c4bcc0
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 16:43:03 -0300

    16:43:03 - Codewars - Challange

--- GreetMe.js
+++ GreetMe.js
@@ -1,4 +1,4 @@
 /*
  https://www.codewars.com/kata/535474308bb336c9980006f2
- 
+ Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
 */
