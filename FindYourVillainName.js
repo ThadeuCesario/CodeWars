3dd238803db9ca6f215559fc57eb4d3fa8fbdc05
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 15:50:31 -0300

    15:50:31 - Codewars - Challange

--- FindYourVillainName.js
+++ FindYourVillainName.js
@@ -11,7 +11,7 @@ function getVillainName(birthday){
   const findUserMonth = birthday.getMonth() + 1;
   const findUserDay = birthday.getDay();
 
-  console.log(typeof findUserDay.length);
+  console.log(typeof findUserDay);
   
 }
 
