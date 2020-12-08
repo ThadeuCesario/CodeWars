c48bb13e3652a616b08997deb46c4c8e8c538f57
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 16:45:05 -0300

    16:45:05 - Codewars - Challange

--- GreetMe.js
+++ GreetMe.js
@@ -3,6 +3,6 @@
  Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
 */
 
-const greet = name => `Hello ${name.}`;
+const greet = name => `Hello ${name.to}`;
 
 greet('riley');
