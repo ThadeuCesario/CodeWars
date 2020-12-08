87212eb22eb57bb8337d4c548e6724a9f6366b63
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 15:38:02 -0300

    15:38:02 - Codewars - Challange

---
+++ FindYourVillainName.js
@@ -0,0 +1,5 @@
+/*
+https://www.codewars.com/kata/536c00e21da4dc0a0700128b/train/javascript
+
+Create a function that returns a villain name based on the user's birthday. The birthday will be passed to the function as a valid Date object, so for simplicity, there's no need to worry about converting strings to dates.
+*/
