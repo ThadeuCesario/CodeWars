595a887be17bad0d74f2b752209120d720d938aa
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 15:53:45 -0300

    15:53:45 - Codewars - Challange

--- FindYourVillainName.js
+++ FindYourVillainName.js
@@ -9,7 +9,7 @@ function getVillainName(birthday){
   const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"]; 
 
   const findUserMonth = birthday.getMonth() + 1;
-  const findUserDay = birthday;
+  const findUserDay = birthday.getDate();
 
   console.log("mes", findUserMonth);
   console.log("dia", findUserDay);
