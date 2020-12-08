da0b74b66371e9ec036e9e174a03ace168884320
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 15:38:58 -0300

    15:38:58 - Codewars - Challange

--- FindYourVillainName.js
+++ FindYourVillainName.js
@@ -3,3 +3,11 @@ https://www.codewars.com/kata/536c00e21da4dc0a0700128b/train/javascript
 
 Create a function that returns a villain name based on the user's birthday. The birthday will be passed to the function as a valid Date object, so for simplicity, there's no need to worry about converting strings to dates.
 */
+
+function getVillainName(birthday){
+  const m = ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing","Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"];
+  const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"];
+  console.log("checking birthday", birthday);
+}
+
+getVillainName(new Date("May 3");
