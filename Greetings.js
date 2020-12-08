684c22e26d5b4f3d6159fc997ce6e1013820b7a2
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 20:57:36 -0300

    20:57:36 - Codewars - Challange

--- Greetings.js
+++ Greetings.js
@@ -1,4 +1,5 @@
 /**
  * https://www.codewars.com/kata/5901c9d463bf406d7c000107
  * 
+ * Greet someone by the given time of day and their name. If no time or name is given, return "Hey dude greet someone"
  */
