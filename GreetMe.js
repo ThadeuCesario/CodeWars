a038dada1dff4f91f9a9ce6ce4d11625ca044a26
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 16:46:53 -0300

    16:46:53 - Codewars - Challange

--- GreetMe.js
+++ GreetMe.js
@@ -3,6 +3,6 @@
  Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
 */
 
-const greet = name => `Hello ${name.toLowerCase()}`;
+const greet = name => `Hello ${name.toLowerCase().charAt(0).toUpperCase()}`;
 
 console.log(greet('THADEU'));
