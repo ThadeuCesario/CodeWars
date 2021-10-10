3ffbd1f04da94131ee2df32d4b24f891fb091a3d
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-10-10 20:53:33 -0300

    SimpleMultiplication Kata

---
+++ SimpleMultiplication.js
@@ -0,0 +1,7 @@
+const simpleMultiplication = (number) => !(number % 2) ? number * 8 : number * 9 
+
+console.log(simpleMultiplication(2));
+console.log(simpleMultiplication(1));
+console.log(simpleMultiplication(8));
+console.log(simpleMultiplication(4));
+console.log(simpleMultiplication(5));
