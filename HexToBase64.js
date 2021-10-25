85b3fd99e9600110bf2ae0ddaa05e22dfb441721
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-10-25 15:15:26 -0300

    Codewars examples

---
+++ HexToBase64.js
@@ -0,0 +1,7 @@
+const hexToDec = (hexString) => hexString.toString(16);
+
+console.log(hexToDec('1'));
+console.log(hexToDec('a'));
+console.log(hexToDec('10'));
+console.log(hexToDec('FF'));
+console.log(hexToDec('-C'));
